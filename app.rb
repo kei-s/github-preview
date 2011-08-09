428600b7c9ca6ffc7c12cbd7621dbc6911593358
kei <kei.shiratsuchi@gmail.com>
2011-08-09 20:21:32 +0900

    add index

--- app.rb
+++ app.rb
@@ -2,7 +2,8 @@ require 'sinatra/base'
 require 'haml'
 
 class Preview < Sinatra::Base
-  set :views, File.dirname(__FILE__) + '/views'
+  set :views,  File.dirname(__FILE__) + '/views'
+  set :public, File.dirname(__FILE__) + '/public'
   get '/' do
     haml :index
   end
