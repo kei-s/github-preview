0b5a25809bdbfdbdf833014a17bc4bc6d5e17663
kei <kei.shiratsuchi@gmail.com>
2011-08-09 20:12:10 +0900

    use haml

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'http://rubygems.org'
 
 gem 'sinatra'
+gem 'haml'
 gem 'github-markup'
--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
 require 'sinatra/base'
+require 'haml'
 
 class Preview < Sinatra::Base
+  set :views, File.dirname(__FILE__) + '/views'
   get '/' do
-    'hi'
+    haml :index
   end
 end
