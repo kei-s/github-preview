46c03841b6fdaddc34b377451a39e2f1c258f95b
kei <kei.shiratsuchi@gmail.com>
2011-08-28 12:01:55 +0900

    sync scroll

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,10 +2,26 @@ $(function(){
   function resize() {
     var height = $(window).height() - $("#header").outerHeight() - 20;
     $("#text").height(height);
-    $("#readme").height(height);
+    $("#preview").height(height);
   }
   resize();
   $(window).resize(_.debounce(resize,300));
+
+  function syncScroll() {
+    var textarea = {
+      size: $('#text')[0].scrollHeight,
+      visibleSize: $('#text').height(),
+      position: $('#text').scrollTop()
+    };
+    var preview = {
+      size: $('#preview')[0].scrollHeight,
+      visibleSize: $('#preview').height(),
+      position: $('#preview').scrollTop()
+    }
+    var ratio = textarea.position / (textarea.size - textarea.visibleSize);
+    $('#preview').scrollTop((preview.size - preview.visibleSize) * ratio);
+  }
+  $('#text').scroll(syncScroll);
 
   function render() {
     $.ajax({
@@ -17,6 +33,7 @@ $(function(){
       },
       success: function(data) {
         $('#preview').empty().append(data);
+        syncScroll();
       }
     });
   }
