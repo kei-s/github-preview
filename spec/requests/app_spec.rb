4f8bb2a2de42e9e58fd9dcc91352a7e7d9843622
kei <kei.shiratsuchi@gmail.com>
2011-08-29 12:54:30 +0900

    add spec by default(markdown)

--- spec/requests/app_spec.rb
+++ spec/requests/app_spec.rb
@@ -3,12 +3,13 @@ require_relative '../spec_helper.rb'
 describe Preview, :js => true do
   it do
     visit('/')
-    page.should have_selector('#text')
+    page.should have_content(:text)
+    page.should have_content(:preview)
   end
 
   it do
     visit('/')
     fill_in('text', :with => '- hi')
-    find('#preview').text.should == "<ul><li>hi</li></ul>"
+    find('#preview ul li').text.should == "hi"
   end
 end
