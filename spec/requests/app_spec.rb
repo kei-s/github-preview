eceb93f5df66fda3e623f2899913c55a90d372f3
kei <kei.shiratsuchi@gmail.com>
2014-04-04 12:49:25 +0900

    Fix style

--- spec/requests/app_spec.rb
+++ spec/requests/app_spec.rb
@@ -1,96 +1,96 @@
 require_relative '../spec_helper.rb'
 
-describe Preview, :js => true do
-  it "shows all elements" do
+describe Preview, js: true do
+  it 'shows all elements' do
     visit('/')
     page.should have_selector('#text')
     page.should have_selector('#preview')
   end
 
-  it "shows markdown preview" do
+  it 'shows markdown preview' do
     visit('/')
-    fill_in('text', :with => '- hi')
-    find('#preview ul li').text.should == "hi"
+    fill_in('text', with: '- hi')
+    find('#preview ul li').text.should == 'hi'
     leave
     accept_confirmation
   end
 
-  context "with submitted text" do
-    it "shows submitted text" do
+  context 'with submitted text' do
+    it 'shows submitted text' do
       visit('/?text=%23XXX')
-      find('#text').text.should == "#XXX"
+      find('#text').text.should == '#XXX'
     end
 
-    it "parses submitted text" do
+    it 'parses submitted text' do
       visit('/?text=%23XXX')
-      find('#preview h1').text.should == "XXX"
+      find('#preview h1').text.should == 'XXX'
     end
 
-    it "parses submitted text in submitted format" do
+    it 'parses submitted text in submitted format' do
       visit('/?text=h1.%20XXX&format=textile')
-      find('#preview h1').text.should == "XXX"
+      find('#preview h1').text.should == 'XXX'
     end
   end
 
-  context "escape" do
-    it "should not show unexpected tag" do
+  context 'escape' do
+    it 'should not show unexpected tag' do
       visit('/?text=</textarea></td><h1>hogehoge</h1>')
       page.should_not have_selector(:xpath, '//body/h1[text()="hogehoge"]')
-      find('#preview h1').text.should == "hogehoge"
+      find('#preview h1').text.should == 'hogehoge'
     end
   end
 
-  describe "formats" do
+  describe 'formats' do
     shared_examples_for 'be well formated' do |format, text|
-      it "should render specified format" do
+      it 'should render specified format' do
         visit('/')
-        select(format, :from => 'format')
-        fill_in('text', :with => text)
-        find('#preview ul li').text.should == "hi"
+        select(format, from: 'format')
+        fill_in('text', with: text)
+        find('#preview ul li').text.should == 'hi'
         leave
         accept_confirmation
       end
     end
 
-    context "markdown" do
+    context 'markdown' do
       it_behaves_like 'be well formated', 'markdown',  '- hi'
     end
 
-    context "textile" do
+    context 'textile' do
       it_behaves_like 'be well formated', 'textile',   '* hi'
     end
 
-    context "rdoc" do
+    context 'rdoc' do
       it_behaves_like 'be well formated', 'rdoc',      '- hi'
     end
 
-    context "org" do
+    context 'org' do
       it_behaves_like 'be well formated', 'org',       '- hi'
     end
 
-    context "creole" do
+    context 'creole' do
       it_behaves_like 'be well formated', 'creole',    '* hi'
     end
 
-    context "mediawiki" do
+    context 'mediawiki' do
       it_behaves_like 'be well formated', 'mediawiki', '* hi'
     end
   end
 
-  context "confirmation after editing" do
+  context 'confirmation after editing' do
     before do
       visit('/')
-      fill_in('text', :with => '- hi')
+      fill_in('text', with: '- hi')
     end
 
-    it "should show when leaving" do
+    it 'should show when leaving' do
       leave
       page.driver.browser.switch_to.alert.text.should_not be_nil
       accept_confirmation
       correct_leaving_destination?
     end
 
-    it "should not show when leaving just after copy text with clippy" do
+    it 'should not show when leaving just after copy text with clippy' do
       click_clippy
       leave
       correct_leaving_destination?
