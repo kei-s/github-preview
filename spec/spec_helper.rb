ba7030493b3b002282d9f2e2fb51f368c4ca8305
Kei Shiratsuchi <kei.shiratsuchi@gmail.com>
2015-11-09 16:27:01 +0900

    Revert 2c0fee48ceb4f29662423e9efe92728671b269d2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,8 @@ require 'bundler'
 Bundler.setup(:test)
 require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
-require 'capybara/poltergeist'
 require 'capybara/rspec'
 require_relative '../app.rb'
 
-Capybara.javascript_driver = :poltergeist
 RSpec.configuration.include Capybara::DSL
 Capybara.app = Preview
